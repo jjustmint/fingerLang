387c76bb6152d547b0bacb31087d294493419baf
jjustmint <123088410+jjustmint@users.noreply.github.com>
2024-06-13 01:18:38 +0700

    update homepage and checklogin

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -22,7 +22,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: MainScreen(),
     );
   }
 }
--- frontend/lib/pages/home/home.dart
+++ frontend/lib/pages/home/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:frontend/pages/home/HomeAppBar.dart';
 import 'package:frontend/pages/home/Textbox.dart';
 import 'package:frontend/pages/home/TopBar.dart';
 import 'package:frontend/pages/home/categoryCard.dart';
@@ -14,46 +17,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFFFCD2),
+      backgroundColor: Color(0xFFFFFCD2),
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            Stack(
-              children: [
-                TopBar(),
-                HomeTextBox(),
-              ],
-            ),
-            Container(
-              child: Padding(
-                padding: EdgeInsets.only(top: 27.0, left: 19.0, right: 19.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Categories',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w800,
-                            color: Colors.black)),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    categoryCard(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    categoryCard(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    categoryCard(),
-                  ],
-                ),
-              ),
-            )
-          ],
+          children: [HomeAppBar(), CategorySection()],
         ),
       ), // Set background color
+    );
+  }
+}
+
+class CategorySection extends StatelessWidget {
+  const CategorySection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 27.0, left: 19.0, right: 19.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Categories',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.black)), // Adjust the height as needed
+          ListView.builder(
+            itemCount: 6,
+            padding: const EdgeInsets.only(
+                top: 24), // Adjust the number of categories as needed
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: categoryCard(),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
