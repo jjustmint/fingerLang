b4572a6c7f8875786851b697328617d5a61e7318
jjustmint <123088410+jjustmint@users.noreply.github.com>
2024-06-22 19:56:21 +0700

    update lessonPage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -25,6 +25,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: MainScreen(),
     );
   }
 }
